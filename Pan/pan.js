7142701ded967a8bd4ca606cc43f4cd67f8243e2
anaiahdiop <anaiahdiop@gmail.com>
2021-12-04 16:37:04 -0800

    all backgrounds in

--- Pan/pan.js
+++ Pan/pan.js
@@ -1,39 +1,126 @@
+//CLASSES
+class Sprite{
+  constructor(image,speedMod,width,height,spriteStates, spriteState, staggerFrames){
+      this.image = image;
+      this.speedMod = speedMod
+      this.width = width;
+      this.height = height;
+      this.spriteAnimations = [];
+      this.staggerFrames = staggerFrames;
+      this.spriteStates = spriteStates;
+      this.spriteState = spriteState;
+
+  }
+
+  findCoordinates(){ // this is for regular sprite sheets
+      this.spriteStates.forEach((state,index)=>{
+          let frames = {
+              loc: []
+          }
+          for(let j = 0; j < state.frames; j++ ){
+              let positionX = j * this.width;
+              let positionY = index * this.height
+              frames.loc.push({x: positionX, y: positionY})
+          }
+          this.spriteAnimations[state.name] = frames;
+      })
+  }
+  
+  update(){ //animate
+      this.findCoordinates()
+      let position = Math.floor(gameFrame/this.staggerFrames) % this.spriteAnimations[this.spriteState].loc.length;
+      let frameX = this.width* position;
+      let frameY = this.spriteAnimations[this.spriteState].loc[position].y;
+      
+      ctx.drawImage(this.image, frameX, frameY, this.width, this.height,200,200,this.width,this.height);
+      // ctx.drawImage(this.image, frameX, frameY, this.width, this.height,0,0,canvas.width,canvas.height);
+      gameFrame++
+      requestAnimationFrame(this.update);
+  }
+};
+
+//VARIABLES
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
-canvas.width = 640;
+canvas.width = 800;
 canvas.height = 360;
+var timeout;
+
+//pop up 
 const moveImg = new Image();
 moveImg.src = './assets/move.png'
 
 
-var timeout;
+//animation
+let gameFrame = 0;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name:'idle',
+        frames:7,
+    },
+    {
+        name:'jump',
+        frames:7,
+    },
+    {
+        name:'fall',
+        frames:7,
+    },
+    {
+        name:'run',
+        frames:9,
+    },
 
+];
+
+var SpriteSheetUrl = './assets/shadow_dog.png'
+var sprite = new Image();
+sprite.src = SpriteSheetUrl;
+const spriteLayer = new Sprite(sprite, 0, 300, 228, animationStates, "run", 10)
+console.log(spriteLayer.findCoordinates())
+
+// const spriteLayer = new Sprite(sprite, 0, 575, 523, animationStates, "run", 10)
+
+
+
+
+//EVENT LISTENERS
 canvas.addEventListener("mousemove", (e) => {
   canvas.style.setProperty('--x', -e.offsetX + "px");
   clearTimeout(timeout);
-  // timeout = setTimeout(function(){alert('MOVE!')}, 1000);
-  timeout = setTimeout(function(){popUp(e.offsetX, e.offsetY)}, 350);
+  timeout = setTimeout(function(){popUp(e.offsetX, e.offsetY)}, 250);
 });
 
 canvas.addEventListener("click", (e) => {
   // ctx.beginPath();
   setTimeout(() => {ctx.clearRect(0,0,canvas.width,canvas.height)},100);
-  canvas.style.setProperty('--height', 370 + "px");
+  canvas.style.setProperty('--height', 515 + "px");
   setTimeout(() => {canvas.style.setProperty('--height', 0 + "px")},1150);
 })
 
+//FUNCTIONS
 function popUp(x,y){
   ctx.drawImage(moveImg, x, y, 100, 100);
-  // ctx.rect(x,y,100,100)
-  // ctx.fillStyle = 'white';
-  //   ctx.fill();
-
-    // setTimeout(() => {ctx.clearRect(0,0,canvas.width,canvas.height)},2000);
-  
 }
+
+function sequence(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+        spriteLayer.update();
+        requestAnimationFrame(sequence);
+};
+
+// sequence();
+
 
 
 
 // e.offsetX is the mouse coordinate for my mouse
 //(name of property , new value)
 
+
+
+// function 
+// go through the frames of my animation 
+//I set the current frame to my background image
+//when i get to last frame change animation state 
--- Running/running.js
+++ Running/running.js
@@ -1,45 +1,15 @@
-// select the canvas and get
-// its 2d context
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 let backgroundSpeed = 10; 
 canvas.width = 600;
 canvas.height = 600;
-// let backgroundFrame = 0;
 
-// SPRITE ANIMATION
-// var sprite = new Image();
-// sprite.src = spriteSheetURL;
-// const spriteWidth = 575;
-// const spriteHeight = 523;
-// let spriteState = 'idle'
 var runSpriteSheetUrl = './assets/firstRun.png'
 var runSprite = new Image();
 runSprite.src = runSpriteSheetUrl;
 
-// stagger frames = how many times it iterates before it moves to next image ->higher # the slower it is
 let gameFrame = 0;
 const spriteAnimations = [];
-const animationStates = [
-    {
-        name:'idle',
-        frames:7,
-    },
-    {
-        name:'jump',
-        frames:7,
-    },
-    {
-        name:'fall',
-        frames:7,
-    },
-    {
-        name:'run',
-        frames:9,
-    },
-
-];
-
 const runAnimationStates = [
     {
         name:'run',
@@ -56,27 +26,13 @@ canvas.addEventListener("mousedown", function() {
 
 
 canvas.addEventListener("mouseup", function() {
+    backgroundSpeed = 10;
     setTimeout(function(){window.location.href = '../Trip/trip.html'}, 100);
-    // backgroundSpeed = 10;
+    
     // showSpeed.innerHTML = backgroundSpeed;
     // spriteLayer.staggerFrames = 10
    
 })
-
-
-//PARALLAX BACKGROUND
-// const backgroundLayer1 = new Image();
-// backgroundLayer1.src = './ForestBackground/02_Bushes.png';
-// const backgroundLayer2 = new Image();
-// backgroundLayer2.src = './ForestBackground/04_Forest.png';
-// const backgroundLayer3 = new Image();
-// backgroundLayer3.src = './ForestBackground/06_Forest.png';
-// const backgroundLayer4 = new Image();
-// backgroundLayer4.src = './ForestBackground/07_Forest.png';
-// const backgroundLayer5 = new Image();
-// backgroundLayer5.src = './ForestBackground/08_Forest.png';
-// const backgroundLayer6 = new Image();
-// backgroundLayer6.src = './ForestBackground/09_Forest.png';
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = './Parallax/layer1.png';
@@ -119,13 +75,15 @@ showSpeed.innerHTML = backgroundSpeed;
       
     };
 
-    class Sprite extends Layer{
+    class Sprite{
         constructor(image,speedMod,width,height,spriteStates, spriteState, staggerFrames){
-            super(image,speedMod)
+            // super(image,speedMod): is there any reason to have them connected
+            this.image = image;
+            this.speedMod = speedMod
             this.width = width;
             this.height = height;
             this.spriteAnimations = [];
-            this.staggerFrames = staggerFrames;
+            this.staggerFrames = staggerFrames; // stagger frames = how many times it iterates before it moves to next image ->higher # the slower it is
             this.spriteStates = spriteStates;
             this.spriteState = spriteState;
 
@@ -154,7 +112,7 @@ showSpeed.innerHTML = backgroundSpeed;
             ctx.drawImage(this.image, frameX, frameY, this.width, this.height,200,325,this.width,this.height);
             // ctx.drawImage(this.image, frameX, frameY, this.width, this.height,0,0,canvas.width,canvas.height);
             gameFrame++
-            //requestAnimationFrame(this.update); //this function tells browser "im going to run an animation" then give the animation "animate"
+            requestAnimationFrame(this.update); //this function tells browser "im going to run an animation" then give the animation "animate"
         }
     };
 
@@ -163,19 +121,11 @@ showSpeed.innerHTML = backgroundSpeed;
     const layer3 = new Layer(backgroundLayer3, 0.6)
     const layer4 = new Layer(backgroundLayer4, 0.5)
     const layer5 = new Layer(backgroundLayer5, 0.3)
-    // const layer6 = new Layer(backgroundLayer6, 0.25)
-    // const spriteLayer = new Sprite(sprite, 0, 575, 523, animationStates, "idle", 5)
     const spriteLayer = new Sprite(runSprite, 0, 200, 200, runAnimationStates, "run", 10)
 
-
-    // Both
     const layers = [layer5,layer4,layer3,
     layer2,spriteLayer,layer1];
 
-    // const layers = [layer6,layer5,layer4,layer3,
-    //     layer2,layer1];
-
-    // const layers = [spriteLayer];
 
 
     function parallax(){ 

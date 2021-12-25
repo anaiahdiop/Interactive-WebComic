390c6c3df349178d759dbbd3efeed0bfd944f701
anaiahdiop <anaiahdiop@gmail.com>
2021-12-24 23:51:19 -0500

    tweaks

--- Pan/pan.js
+++ Pan/pan.js
@@ -54,29 +54,60 @@ backgroundLayer17.src = './assets/Transformation/transform17.png';
 const backgroundLayer18 = new Image();
 backgroundLayer18.src = './assets/Transformation/transform18.png';
 
+console.log(backgroundLayer1)
 const transformationStates = [
     {
         name:'first',
         frames: ['url(./assets/Transformation/transform1.png)','url(./assets/Transformation/transform2.png)','url(./assets/Transformation/transform3.png)','url(./assets/Transformation/transform4.png)','url(./assets/Transformation/transform5.png)','url(./assets/Transformation/transform6.png)','url(./assets/Transformation/transform7.png)','url(./assets/Transformation/transform8.png)','url(./assets/Transformation/transform9.png)',],
-        // frames: ['url(./assets/1.png)','url(./assets/2.png)','url(./assets/3.png)'],
+        // frames: [backgroundLayer1,backgroundLayer2,backgroundLayer3,backgroundLayer4,backgroundLayer5,backgroundLayer6,backgroundLayer7,backgroundLayer8,backgroundLayer9],
     },
     {
         name:'second',
         frames: ['url(./assets/Transformation/transform10.png)','url(./assets/Transformation/transform11.png)','url(./assets/Transformation/transform12.png)','url(./assets/Transformation/transform13.png)','url(./assets/Transformation/transform14.png)','url(./assets/Transformation/transform14.png)','url(./assets/Transformation/transform15.png)','url(./assets/Transformation/transform16.png)','url(./assets/Transformation/transform17.png)'],
-        // frames: ['url(./assets/4.png)','url(./assets/5.png)','url(./assets/6.png)'],
+        // frames: [backgroundLayer10,backgroundLayer11,backgroundLayer12,backgroundLayer13,backgroundLayer14,backgroundLayer14,backgroundLayer15,backgroundLayer16,backgroundLayer17],
     },
-  //   {
-  //     name:'third',
-  //     frames: ['url(./assets/Transformation/transform13.png)','url(./assets/Transformation/transform14.png)','url(./assets/Transformation/transform15.png)','url(./assets/Transformation/transform16.png)','url(./assets/Transformation/transform17.png)','url(./assets/Transformation/transform18.png)'],
-  //     // frames: ['url(./assets/4.png)','url(./assets/5.png)','url(./assets/6.png)'],
-  // }
-
 ];
 
 //EVENT LISTENERS
 window.addEventListener('load', (event) => {
   sequence();
   ctx.drawImage(indicator, 0, 0, 75, 75)
+  const backgroundLayer1 = new Image();
+backgroundLayer1.src = './assets/Transformation/transform1.png';
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = './assets/Transformation/transform2.png';
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = './assets/Transformation/transform3.png';
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = './assets/Transformation/transform4.png';
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = './assets/Transformation/transform5.png';
+const backgroundLayer6 = new Image();
+backgroundLayer6.src = './assets/Transformation/transform6.png';
+const backgroundLayer7 = new Image();
+backgroundLayer7.src = './assets/Transformation/transform7.png';
+const backgroundLayer8 = new Image();
+backgroundLayer8.src = './assets/Transformation/transform8.png';
+const backgroundLayer9 = new Image();
+backgroundLayer9.src = './assets/Transformation/transform9.png';
+const backgroundLayer10 = new Image();
+backgroundLayer10.src = './assets/Transformation/transform10.png';
+const backgroundLayer11 = new Image();
+backgroundLayer11.src = './assets/Transformation/transform11.png';
+const backgroundLayer12 = new Image();
+backgroundLayer12.src = './assets/Transformation/transform12.png';
+const backgroundLayer13 = new Image();
+backgroundLayer13.src = './assets/Transformation/transform13.png';
+const backgroundLayer14 = new Image();
+backgroundLayer14.src = './assets/Transformation/transform14.png';
+const backgroundLayer15 = new Image();
+backgroundLayer15.src = './assets/Transformation/transform15.png';
+const backgroundLayer16 = new Image();
+backgroundLayer16.src = './assets/Transformation/transform16.png';
+const backgroundLayer17 = new Image();
+backgroundLayer17.src = './assets/Transformation/transform17.png';
+const backgroundLayer18 = new Image();
+backgroundLayer18.src = './assets/Transformation/transform18.png';
 });
 
 document.addEventListener("mousemove", (e) => {
@@ -163,7 +194,6 @@ function distance(x1,y1,x2,y2){
   return c;
 } 
 
-console.log(distance(5,6,7,1))
 let popUps = [];
 
 function popUp(x,y){
@@ -178,11 +208,8 @@ function popUp(x,y){
   if(popUps.length === 0){
     popUps.push(pop);
   }
-  console.log("this is pop " + pop.x,pop.y);
   for(let i=0; i<popUps.length;i++){
-      console.log(popUps[i].x,popUps[i].y);
       let d = distance(pop.x,pop.y,popUps[i].x,popUps[i].y)
-      // console.log(`this is the distance ${d}  this is the addition  ${pop.r + popUps[i].r}`)
       if(d < pop.r + popUps[i].r){
         overlap = true;
         break;
@@ -192,7 +219,6 @@ function popUp(x,y){
         popUps.push(pop);
       }
   }
-  console.log(popUps)
   // popUps.push(pop)
   ctx.drawImage(popUps[popUps.length - 1].img, popUps[popUps.length - 1].x, popUps[popUps.length - 1].y, 100, 100);
 
